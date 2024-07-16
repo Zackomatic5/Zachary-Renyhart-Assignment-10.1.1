fbaad462e4517074d91bc295fb57750c27c1a91e
Zachary Renyhart <Zachary.Renyhart@mssa.newhorizons.com>
2024-07-16 15:51:27 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System.Data;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+
+namespace Zachary_Renyhart_Assignment_10._1._1
+    
+{
+    
+    internal class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            {
+                student.StudentId = 1567239;
+                student.FirstName = "Zachary";
+                student.LastName = "Renyhart";
+                student.YearBorn = 2000;
+                student.StudentGrade = 'A';
+                student.GPA = 3.6;
+            }
+            string jsonString = JsonSerializer.Serialize(student);
+            //The resulting JSON string is stored in the jsonString variable.
+            Console.WriteLine(jsonString);
+            Console.WriteLine();
+
+            
+
+            string fileName = "Student.json";
+            //string fileName = "Student.json"; assigns the filename to the variable fileName.
+            string jsonString1 = JsonSerializer.Serialize(student);
+            File.WriteAllText(fileName, jsonString1);
+            // writes the contents of jsonString1 to the file specified by fileName.
+            Console.WriteLine(File.ReadAllText(fileName));
+            //reads the contents of the “Student.json” file and prints it to the console.
+
+
+            XmlSerializer ser = new XmlSerializer(typeof(Student));
+            DataSet ds = new DataSet("mydadaSet");
+            DataTable t = new DataTable("table1");
+            DataColumn c = new DataColumn("thing");
+            t.Columns.Add(c);
+            ds.Tables.Add(t);
+            DataRow r;
+
+            for (int i = 0; i < 10; i++)
+            {
+                r = t.NewRow();
+                r[0] = "Thing" + i;
+                t.Rows.Add(r);
+            }
+            TextWriter writer = new StreamWriter(fileName);
+            ser.Serialize(writer, ds);
+            writer.Close();
+
+        }
+
+        
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zachary_Renyhart_Assignment_10._1._1
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string LastName { get; set; }
+        public double GPA { get; set; }
+        public char StudentGrade { get; set; }
+        private string _firstName { get; set; }
+        public string FirstName
+        {
+            get { return _firstName; }
+
+            set { _firstName = value; }
+        }
+        private int _yearBorn;
+        public int YearBorn
+        {
+            get { return _yearBorn; }
+
+            set { _yearBorn = value; }
+        }
+
+    
+
+        
+        //This is a class constructor
+    public Student()
+        {
+            //This is initializing the values of the class above
+            StudentId = 0;
+            FirstName = "Zack";
+            YearBorn = 0;
+            LastName = "Renyhart";
+            GPA = 1.0;
+            StudentGrade = 'B';
+            
+        }
+
+    }
+
+  
+}
